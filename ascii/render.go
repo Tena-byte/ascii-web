120c83d60d9e168884ce43ed8609f7354a5fa4c4
emmuloko <emmanuellauloko@gmail.com>
2026-06-05 04:34:26 +0100

    ascii web

--- ascii/render.go
+++ ascii/render.go
@@ -1,18 +1,17 @@
 package ascii
 
+import "strings"
 
-
-
-func Render(text string, font map[rune][]string) []string {
+func Render(text string, font map[rune][]string) string {
 
 	result := make([]string, 8)
 
-	for _, ch := range text{
+	for _, ch := range text {
 
-		for row := 0; row < 8; row++{
+		for row := 0; row < 8; row++ {
 
 			result[row] += font[ch][row]
 		}
 	}
-	return result
+	return strings.Join(result, "\n")
 }
--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,21 @@
 package handler
+
+import (
+	"net/http"
+	"ascii-web/ascii"
+)
+func Generate(w http.ResponseWriter, r *http.Request) {
+
+	text := r.FormValue("text")
+	banner := r.FormValue("banner")
+
+	result := ascii.Generate(text, banner)
+
+	data := struct {
+		Result string
+	}{
+		Result: result,
+	}
+
+	tmpl.Execute(w, data)
+}
